0024c3968a5cb028682753a386a34a57dc9ebc3e
Bartek <bartek.koszek@gmail.com>
2021-04-12 23:39:58 +0200

    poprawka bledu w funkcji get_info()

--- kalkulator.py
+++ kalkulator.py
@@ -1,5 +1,7 @@
+def get_info()
+print("Witaj, to jest prosty kalkulator!")
 def dodaj():
-    print("Witaj, to jest prosty kalkulator!")
+    print("Wprowadz dwie liczby, a zostana one dodane")
     a = int(input())
     b = int(input())
     print(a  + b)
